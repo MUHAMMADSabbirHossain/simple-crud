e7a5888550929bf41bd470a39589a00e7d648c56
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-05-28 14:34:23 +0600

    frontend: Root.jsx updated...

--- simple-crud-frontend/src/Root/Root.jsx
+++ simple-crud-frontend/src/Root/Root.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
+import App from "../App";
 
 const Root = () => {
     return (
         <div>
+            <h1>Simple CRUD</h1>
+            <App></App>
             <Outlet></Outlet>
         </div>
     );
